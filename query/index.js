649fb074904b40d9450188b7730af7b48e90d845
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2022-08-18 17:40:10 -0500

    microservices from scratch

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,27 +9,21 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
-    if(type === 'PostCreated') {
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
         const { id, title } = data;
 
         posts[id] = { id, title, comments: [] };
     }
 
-    if(type === 'CommentCreated') {
+    if (type === 'CommentCreated') {
         const { id, content, postId, status } = data;
 
         const post = posts[postId];
         post.comments.push({ id, content, status });
     }
 
-    if(type === 'CommentUpdated') {
+    if (type === 'CommentUpdated') {
         const { id, content, postId, status } = data;
 
         const post = posts[postId];
@@ -39,12 +34,28 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on port 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data) {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
