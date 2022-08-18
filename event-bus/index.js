01c001d1f6ed46c41402972f454e4868bcaff409
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2022-08-18 17:07:38 -0500

    first iteration of app

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,8 +17,11 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4002/events', event).catch((err) => {
         console.log(err.message);
     });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
-    res.send({status: 'OK'});
+    res.send({ status: 'OK' });
 });
 
 app.listen(4005, () => {
